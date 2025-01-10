316ef84ecd8d8b9d3eef4c99f2b90ea465bd0f4b
Eddy GASANA <e.gasana@alustudent.com>
2025-01-10 22:48:31 +0200

    Second Update on test.py

--- test.py
+++ test.py
@@ -22,3 +22,34 @@ B =np.array([[1, 2, 3, 4, 5],
               [1, 2, 3, 4, 5]])
 
    
+#Implement the formula MX + B
+#Have two function one using numpy and another not using numpy
+#Return 
+@app.post("/calculate")
+async def f(request: Request):
+    body = await request.json()
+
+    matrix = body.get("matrix")
+
+    if not matrix or len(matrix) != 5 or any(len(row) != 5 for row in matrix):
+        return {"error": "Matrix must be 5x5"}
+
+
+    X = np.array(matrix)
+
+    print(X)
+
+    # Calculate using numpy
+    numpy_result = with_numpy(M, X, B)
+    
+    # Calculate without numpy
+    non_numpy_result = without_numpy(M, B, X)
+
+    # Apply sigmoid function
+    sigmoid_result = sigmoid(numpy_result)
+
+    return {
+        "matrix_multiplication": numpy_result.tolist(),
+        "non_numpy_multiplication": non_numpy_result,
+        "sigmoid_output": sigmoid_result.tolist()
+    }
