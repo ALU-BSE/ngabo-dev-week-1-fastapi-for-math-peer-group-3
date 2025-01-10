f08d622ede6f4a77b7c1d4b4a4d8ccadb991241c
Duel <139622859+Irenee123@users.noreply.github.com>
2025-01-10 13:28:42 -0800

    Update test.py

--- test.py
+++ test.py
@@ -53,3 +53,34 @@ async def f(request: Request):
         "non_numpy_multiplication": non_numpy_result,
         "sigmoid_output": sigmoid_result.tolist()
     }
+    
+# Function to calculate the matrix multiplication using numpy
+def with_numpy(M, X, B):
+    return np.matmul(M, X) + B
+
+# Function to calculate the matrix multiplication without using numpy
+def without_numpy(mat1, X, mat2):
+    """initialized a matrix of 0 to store solutions"""
+    solution = [[0 for _ in range(5)] for _ in range(5)]
+    """nested loop to iterate through the matrices rows and columns"""
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+        raise ValueError("Invalid Matrix Dimensions")
+    else:
+        for i in range(5):                  #rows of M
+            for j in range(5):              #rows of B
+                for k in range(5):          #multiplication and addition
+                    solution[i][j] += int(mat1[i][k] * X[k][j])
+                solution[i][j] += int(mat2[i][j])
+    return solution
+
+# Function to apply the sigmoid function
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
+
+'''
+    Create a requirements.txt
+    Upload to render
+'''
