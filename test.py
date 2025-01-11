67d220ab7518b95d3cd6fc04f93ce8d26ea95582
Irenee123 <i.dusingizi@alustudent.com>
2025-01-10 21:58:27 -0800

    Testing update

--- test.py
+++ test.py
@@ -18,9 +18,7 @@ def f(x):
 
 
 #initialize x as a 5 * 5 matrix
-with open("example.txt", "r") as file:
-    line_count = sum(1 for line in file)
-    print(f"Number of lines: {line_count}")
+
 
 #Make a call to the function
 
